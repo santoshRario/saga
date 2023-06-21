8439c1c8893b6a9c2c772c201c2f55460bb52414
santoshRario <santosh.punase@rario.com>
2023-06-21 19:23:09 +0530

    added cover image url

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -9,6 +9,7 @@ async function BlogPage({ params }: { params: { slug: string } }) {
       title
       summary
       isPublished
+      coverImageUrl
     }
   }`)
 
@@ -17,9 +18,9 @@ async function BlogPage({ params }: { params: { slug: string } }) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between py-24 mx-auto w-96">
       <div className='pb-12'>
-        <h1 className='text-2xl'>{blog.title}</h1>
-        <Image src={blog.coverImageUrl} width={100} height={300} alt={'Image'} />
-        <span className='text-sm'>{blog.summary}</span>
+        <h1 className='text-2xl mb-12'>{blog.title}</h1>
+        <Image src={blog.coverImageUrl} height={300} width={400} alt={'Image'} />
+        <h4 className='text-sm mt-10'>{blog.summary}</h4>
       </div>
     </main>
   )
